e31ca4896f73d4b7761bab25f3cac9f8e2a0f6e1
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-09-03 15:11:14 -0400

    Create middleoflinkedlist.py

---
+++ middleoflinkedlist.py
@@ -0,0 +1,47 @@
+# 876. Middle of the Linked List
+# Easy
+
+# 6772
+
+# 179
+
+# Add to List
+
+# Share
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+# Example 2:
+
+
+# Input: head = [1,2,3,4,5,6]
+# Output: [4,5,6]
+# Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        store = []
+        while head is not None:
+            store.append(head)
+            head = head.next
+        rest = (len(store) // 2)
+        return store[rest]
