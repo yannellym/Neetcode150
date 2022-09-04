6194ae185905bfcc4f9f53f34fd8a3c8e76be455
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-09-04 16:47:45 -0400

    Create mergenodesbetweenzero.py

---
+++ mergenodesbetweenzero.py
@@ -0,0 +1,79 @@
+# 2181. Merge Nodes in Between Zeros
+# Medium
+
+# 747
+
+# 19
+
+# Add to List
+
+# Share
+# You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+
+# For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+
+# Return the head of the modified linked list.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [0,3,1,0,4,5,2,0]
+# Output: [4,11]
+# Explanation: 
+# The above figure represents the given linked list. The modified list contains
+# - The sum of the nodes marked in green: 3 + 1 = 4.
+# - The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+# Example 2:
+
+
+# Input: head = [0,1,0,3,0,2,2,0]
+# Output: [1,3,4]
+# Explanation: 
+# The above figure represents the given linked list. The modified list contains
+# - The sum of the nodes marked in green: 1 = 1.
+# - The sum of the nodes marked in red: 3 = 3.
+# - The sum of the nodes marked in yellow: 2 + 2 = 4.
+ 
+
+# Constraints:
+
+# The number of nodes in the list is in the range [3, 2 * 105].
+# 0 <= Node.val <= 1000
+# There are no two consecutive nodes with Node.val == 0.
+# The beginning and end of the linked list have Node.val == 0.
+# Accepted
+# 47,684
+# Submissions
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeNodes(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+                
+#    
+    
+        dummy_head = ListNode(-1)
+        newlist_current = dummy_head
+        current = head
+        
+        while current is not None:
+            current = current.next
+            counter = 0
+            while current is not None and current.val != 0:
+                counter += current.val
+                current = current.next
+            if counter != 0:
+                newlist_current.next = ListNode(counter)
+                newlist_current = newlist_current.next
+        return dummy_head.next
+        
