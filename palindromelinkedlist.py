1cfb10d993860908151dc0e0cf93b877fc7c3ffc
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-10-05 11:10:37 -0400

    Create palindromelinkedlist.py

---
+++ palindromelinkedlist.py
@@ -0,0 +1,43 @@
+# Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,2,1]
+# Output: true
+# Example 2:
+
+
+# Input: head = [1,2]
+# Output: false
+ 
+
+# Constraints:
+
+# The number of nodes in the list is in the range [1, 105].
+# 0 <= Node.val <= 9
+ 
+  
+  # Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        store = ""
+        
+        cur = head
+        
+        while cur:
+            store += str(cur.val)
+            cur = cur.next
+        
+        return store == ''.join(reversed(store))
+        
