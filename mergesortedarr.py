184b9430691722658f25e2898989add01f0273ad
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2022-12-05 17:50:30 -0500

    Create mergesortedarr.py

---
+++ mergesortedarr.py
@@ -0,0 +1,41 @@
+# 88. Merge Sorted Array
+# Easy
+
+# 8502
+
+# 760
+
+# Add to List
+
+# Share
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+# To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        # start merging from the ends of the lists
+        p1, p2 = m-1, n-1
+        # destination d index to insert into nums1
+        for d in range(m+n-1, -1, -1):
+            # if second list is empty, nothing more to merge
+            if p2 < 0:
+                return
+            # only merge from nums1 if there are items left to merge
+            # insert at d the larger of the two values from nums1 and nums2
+            if p1 >= 0 and nums1[p1] > nums2[p2]:                
+                nums1[d] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[d] = nums2[p2]
+                p2 -= 1
