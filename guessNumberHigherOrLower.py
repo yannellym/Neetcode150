532f8e87809dd7ff04b3ceaf9c78277c5de1c8ed
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-01-10 17:01:23 -0500

    Create guessNumberHigherOrLower.py

---
+++ guessNumberHigherOrLower.py
@@ -0,0 +1,63 @@
+# 374. Guess Number Higher or Lower
+# Easy
+# 2.6K
+# 312
+# Companies
+# We are playing the Guess Game. The game is as follows:
+
+# I pick a number from 1 to n. You have to guess which number I picked.
+
+# Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+# You call a pre-defined API int guess(int num), which returns three possible results:
+
+# -1: Your guess is higher than the number I picked (i.e. num > pick).
+# 1: Your guess is lower than the number I picked (i.e. num < pick).
+# 0: your guess is equal to the number I picked (i.e. num == pick).
+# Return the number that I picked.
+
+ 
+
+# Example 1:
+
+# Input: n = 10, pick = 6
+# Output: 6
+# Example 2:
+
+# Input: n = 1, pick = 1
+# Output: 1
+# Example 3:
+
+# Input: n = 2, pick = 1
+# Output: 1
+ 
+
+# Constraints:
+
+# 1 <= n <= 231 - 1
+# 1 <= pick <= n
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low = 0
+        high = n
+
+        while low <= high:
+            middle = (low+high) // 2
+            if guess(middle) == -1:
+                high = middle - 1
+            elif guess(middle) == 1:
+                low = middle +1
+            else:
+                return middle
