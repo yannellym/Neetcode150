b15f6dbef0b63b60a8cbe15f383763f5e5be308e
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-01-31 13:09:27 -0500

    Create insertIntoaBinarySearchTree.py

---
+++ insertIntoaBinarySearchTree.py
@@ -0,0 +1,78 @@
+# 701. Insert into a Binary Search Tree
+# Medium
+# 4.5K
+# 156
+# Companies
+# You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+# Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [4,2,7,1,3], val = 5
+# Output: [4,2,7,1,3,5]
+# Explanation: Another accepted tree is:
+
+# Example 2:
+
+# Input: root = [40,20,60,10,30,50,70], val = 25
+# Output: [40,20,60,10,30,50,70,null,null,25]
+# Example 3:
+
+# Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+# Output: [4,2,7,1,3,5]
+ 
+
+# Constraints:
+
+# The number of nodes in the tree will be in the range [0, 104].
+# -108 <= Node.val <= 108
+# All the values Node.val are unique.
+# -108 <= val <= 108
+# It's guaranteed that val does not exist in the original BST.
+# Accepted
+# 384.1K
+# Submissions
+# 516K
+# Acceptance Rate
+# 74.4%
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # if there is no root, return a node with val inside
+        if not root:
+            return TreeNode(val)
+        # helper function to insert
+        def insert(node):
+            # if the val is greater than the current node's value
+            if val > node.val:
+                # if there is no node in the right
+                if not node.right:
+                    # insert a new node to the right
+                    node.right = TreeNode(val)
+                else:
+                    # if there is a node node to the right, call insert func again w/ node.right
+                    insert(node.right)
+            else:
+                # if there is no node in the left
+                if not node.left:
+                    # insert a new node to the left
+                    node.left = TreeNode(val)
+                else:
+                    # if there is a node node to the left, call insert func again w/ node.left
+                    insert(node.left)
+        #call helper function insert with the root as a value
+        insert(root)
+        # return the root as we want the entire tree
+        return root
+
+        # the key to solving this problem is knowing that you can insert it at the bottom of the leaf node.
