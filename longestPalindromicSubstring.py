33d83fd27cd46b50a6d6d7dac050ef8832f1c87a
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-02-11 13:56:54 -0500

    Create longestPalindromicSubstring.py

---
+++ longestPalindromicSubstring.py
@@ -0,0 +1,59 @@
+#  Longest Palindromic Substring
+# Medium
+# 23.8K
+# 1.4K
+# Companies
+# Given a string s, return the longest 
+# palindromic
+ 
+# substring
+#  in s.
+
+ 
+
+# Example 1:
+
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+# Example 2:
+
+# Input: s = "cbbd"
+# Output: "bb"
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consist of only digits and English letters.
+# Accepted
+# 2.3M
+# Submissions
+# 7.1M
+# Acceptance Rate
+32.4%
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        size = len(s)
+        # while size > 0 
+        while(size>0):
+            # loop length of s - (size + 1) times
+            for i in range(len(s) - size + 1):
+                # x will equal the substring of s from i to i plus size
+                x = s[i:i+size]
+                # if x equals the reverse of x return x
+                # this returns the first palindromic substring it sees.
+                # since're we starting with the entire array, it will automatically return the longest one
+                if x == x[::-1]:
+                    return x
+            # decrement size
+            size -= 1
+
+        
