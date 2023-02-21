4e4089ff528b25ed44d9fd091df9f33c02457efa
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-02-21 13:20:40 -0500

    Update findthetwonjudge.py

--- findthetwonjudge.py
+++ findthetwonjudge.py
@@ -45,24 +45,24 @@
 # Acceptance Rate
 # 49.7%
 
-class Solution(object):
-    def findJudge(self, n, trust):
-        """
-        :type n: int
-        :type trust: List[List[int]]
-        :rtype: int
-        """
-        
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
         trust_count=[0]*(n+1)
-    
-
+        
         for person1,person2 in trust:
+            # is trusting someone so -1
             trust_count[person1]-=1
+            # is trusted by someone so +1 
             trust_count[person2]+=1
-            print(trust_count)
+        print(trust_count)
+
+        # loop 1 through n +1
         for i in range(1,n+1):
+            # if the trust count of the person is equal to n-1 :
+            # n - 1 because we are counting everyone but the judge
+            # if n = 3 we should check if the judge has a count of 2
             if trust_count[i]==n-1:
+                # return the judge
                 return i
-            
-        return -1  
-		
+        # if none of the people meet that condition, return -1
+        return -1 
