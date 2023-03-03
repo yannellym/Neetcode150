326966177d05875cee6a1e478ef2cbbe4ae1b601
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-03-02 19:16:03 -0500

    Create sortThePeople.py

---
+++ sortThePeople.py
@@ -0,0 +1,60 @@
+# 2418. Sort the People
+# Easy
+# 728
+# 10
+# Companies
+# You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+# For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+# Return names sorted in descending order by the people's heights.
+
+ 
+
+# Example 1:
+
+# Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+# Output: ["Mary","Emma","John"]
+# Explanation: Mary is the tallest, followed by Emma and John.
+# Example 2:
+
+# Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+# Output: ["Bob","Alice","Bob"]
+# Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+ 
+
+# Constraints:
+
+# n == names.length == heights.length
+# 1 <= n <= 103
+# 1 <= names[i].length <= 20
+# 1 <= heights[i] <= 105
+# names[i] consists of lower and upper case English letters.
+# All the values of heights are distinct.
+# Accepted
+# 63.6K
+# Submissions
+# 78.3K
+# Acceptance Rate
+# 81.2%
+
+class Solution(object):
+    def sortPeople(self, names, heights):
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
+        store = dict()
+
+        for i in range(len(names)):
+            store[i] = heights[i]
+            
+        store = sorted(store, key=lambda x: -store[x])
+        res = []
+
+        for i in store:
+            res.append(names[i])
+        return res
+
+        
