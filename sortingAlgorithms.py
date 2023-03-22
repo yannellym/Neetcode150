0f95c03cffc73ebd96cf5cec5695d3e11d7c1f83
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-03-22 10:44:12 -0400

    Update sortingAlgorithms.py

--- sortingAlgorithms.py
+++ sortingAlgorithms.py
@@ -48,3 +48,67 @@
             (nums[step], nums[min_idx]) = (nums[min_idx], nums[step])
         return nums
         '''
+        
+        # merge sort - O(n(log(n)))
+        '''
+         def merge(arr, L, M, R):
+            # get the left, and right array (so we don't modify nums)
+            left, right = arr[L:M+1], arr[M+1:R+1]
+            # create 3 pointers, 
+            # i will point to nums, j starts at 0(of left), and k at 0(of left)
+            i, j, k = L, 0, 0
+            # while both pointers are within bounds
+            while j < len(left) and k < len(right):
+                # if the number at the left is less than or equal to the one on the right
+                if left[j] <= right[k]:
+                    # have the number in arr equal the number on the left
+                    arr[i] = left[j]
+                    # increase the left pointer
+                    j +=1
+                else:
+                    # have the number in arr equal the number on the right
+                    arr[i] = right[k]
+                    # increase the right pointer
+                    k+=1
+                # increase i regardless because we want to look at all the digits in arr
+                i+=1
+            # if we still have digits left from our splitted arrs, lets merge them
+
+            # if we have digits in the left arr, we need to address this
+            # one of these while loops will execute
+            
+            # while j is less than left
+            while j <len(left):
+                # have the digit at arr equal the digit at left
+                arr[i] = left[j]
+                # increase i to move on to the next digit in left
+                j += 1
+                # increase i to move on to the next digit in arr
+                i += 1
+            while k <len(right):
+                # have the digit at arr equal the digit at right
+                arr[i] = right[k]
+                # increase j to move on to the next digit in right
+                k+=1
+                # increase i to move on to the next digit in arr
+                i +=1
+
+        def mergeSort(arr, l, r):
+            # if our arr is of length 1: return the arr
+            if l == r:
+                return arr
+
+            # get the mid of the arr
+            m = (l+r) //2
+            # call mergeSort on the left half of array
+            mergeSort(arr, l, m)
+            # call mergeSort on the right half of array
+            mergeSort(arr, m+1, r)
+            # call the merge func on the arr by passing in the l, m, and r.
+            merge(arr, l, m, r)
+            # return the merged array
+            return arr
+        # call the mergerSort helper func:
+        # pass in the arr, 0 to start, and len of nums-1 as the end
+        return mergeSort(nums, 0, len(nums)-1)
+      '''
