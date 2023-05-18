5aad0c140ab072e36f49b462e7f379b066bafba8
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-05-17 20:33:18 -0400

    Create medianOfTwoSortedArrays.py

---
+++ medianOfTwoSortedArrays.py
@@ -0,0 +1,60 @@
+4. Median of Two Sorted Arrays
+Hard
+23.4K
+2.6K
+Companies
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+Accepted
+1.9M
+Submissions
+5.2M
+Acceptance Rate
+36.4%
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+
+        nums1.extend(nums2)
+        nums1.sort()
+
+  
+
+        n = len(nums1)
+
+        even = True if n % 2 == 0  else False
+
+
+        if even:
+            return float(nums1[(n//2)] + nums1[(n//2)-1]) / 2
+        else:
+            return nums1[n/2]
