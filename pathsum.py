cf52cabe23a73b97d46cff8868e4f972b15de35a
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-07-14 18:46:28 -0400

    Update pathsum.py

--- pathsum.py
+++ pathsum.py
@@ -69,3 +69,31 @@ class Solution:
             return (dfs(node.left, curSum) or dfs(node.right, curSum))
         # call your helper function
         return dfs(root, 0)
+
+
+# alternative 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: bool
+        """
+        # if there's no root, return False
+        if root is None:
+            return False
+        # if the node is a leaf node, check if the value is equal to targetSum
+        # You;re on the last node and it hasn't been subtracted yet so if there;s a path sum
+        # targetSym and root.val will equal each other
+        if root.left is None and root.right is None:
+            return targetSum == root.val
+
+        # return if either one is true. 
+        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
