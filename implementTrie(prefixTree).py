dcd1b759f0b4683f38e75ba0776f7a0a28e4e455
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-07-17 18:21:40 -0400

    Update implementTrie(prefixTree).py

--- implementTrie(prefixTree).py
+++ implementTrie(prefixTree).py
@@ -48,6 +48,46 @@
 # Acceptance Rate
 # 63.3%
 
+
+#actual trie
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_word = True
+
+    def search(self, word: str) -> bool:
+        return self.searchHelper(word, self.root)
+
+    def searchHelper(self, word: str, node: TrieNode) -> bool:
+        for i, char in enumerate(word):
+            if char == '.':
+                for child in node.children.values():
+                    if self.searchHelper(word[i + 1:], child):
+                        return True
+                return False
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_word
+
+
+
+
+
+# alternative
 class Trie(object):
 
     def __init__(self):
