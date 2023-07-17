470df93fee470ab1a38f9d5a65f27e4d215f4c61
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-07-17 19:03:49 -0400

    Update implementTrie(prefixTree).py

--- implementTrie(prefixTree).py
+++ implementTrie(prefixTree).py
@@ -51,38 +51,38 @@
 
 #actual trie
 
-class TrieNode:
+class Node:
     def __init__(self):
-        self.children = {}
-        self.is_word = False
+        self.children = {}  # Dictionary to store child nodes
+        self.is_word = False  # Flag to indicate if a word ends at this node
 
-class WordDictionary:
+class Trie:
     def __init__(self):
-        self.root = TrieNode()
+        self.root = Node()  # Initialize the root node of the Trie
 
-    def addWord(self, word: str) -> None:
-        node = self.root
+    def insert(self, word: str) -> None:
+        node = self.root  # Start from the root node
         for char in word:
             if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.is_word = True
+                node.children[char] = Node()  # Create a new node if the character is not present
+            node = node.children[char]  # Move to the next node
+        node.is_word = True  # Mark the last node as the end of a word
 
     def search(self, word: str) -> bool:
-        return self.searchHelper(word, self.root)
-
-    def searchHelper(self, word: str, node: TrieNode) -> bool:
-        for i, char in enumerate(word):
-            if char == '.':
-                for child in node.children.values():
-                    if self.searchHelper(word[i + 1:], child):
-                        return True
-                return False
+        node = self.root  # Start from the root node
+        for char in word:
             if char not in node.children:
-                return False
-            node = node.children[char]
-        return node.is_word
+                return False  # Return False if the character is not found
+            node = node.children[char]  # Move to the next node
+        return node.is_word  # Return True if the end of the word is reached
 
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root  # Start from the root node
+        for char in prefix:
+            if char not in node.children:
+                return False  # Return False if the character is not found
+            node = node.children[char]  # Move to the next node
+        return True  # Return True if the end of the prefix is reached
 
 
 
