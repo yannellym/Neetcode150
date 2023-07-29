57e00dbd530d8a004f699363be34a0c449c04904
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-07-29 19:57:14 -0400

    Create rising_temperature.sql

---
+++ rising_temperature.sql
@@ -0,0 +1,68 @@
+197. Rising Temperature
+Easy
+1.9K
+504
+company
+Amazon
+company
+Adobe
+company
+Bloomberg
+SQL Schema
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+Accepted
+375.8K
+Submissions
+832.5K
+Acceptance Rate
+45.1%
+
+
+# Write your MySQL query statement below
+
+SELECT w1.id
+FROM Weather AS w1
+JOIN Weather AS w2 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+AND w1.temperature > w2.temperature;
