77f001b215e39f384a6f76ee0f4812004ca295bc
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-08-08 13:34:16 -0400

    Create isTreeSymmetric.py

---
+++ isTreeSymmetric.py
@@ -0,0 +1,124 @@
+Given a binary tree t, determine whether it is symmetric around its center, i.e. each side mirrors the other.
+
+Example
+
+For
+
+t = {
+    "value": 1,
+    "left": {
+        "value": 2,
+        "left": {
+            "value": 3,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 4,
+            "left": null,
+            "right": null
+        }
+    },
+    "right": {
+        "value": 2,
+        "left": {
+            "value": 4,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 3,
+            "left": null,
+            "right": null
+        }
+    }
+}
+the output should be solution(t) = true.
+
+Here's what the tree in this example looks like:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+As you can see, it is symmetric.
+
+For
+
+t = {
+    "value": 1,
+    "left": {
+        "value": 2,
+        "left": null,
+        "right": {
+            "value": 3,
+            "left": null,
+            "right": null
+        }
+    },
+    "right": {
+        "value": 2,
+        "left": null,
+        "right": {
+            "value": 3,
+            "left": null,
+            "right": null
+        }
+    }
+}
+the output should be solution(t) = false.
+
+Here's what the tree in this example looks like:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+As you can see, it is not symmetric.
+
+Input/Output
+
+[execution time limit] 4 seconds (py)
+
+[memory limit] 1 GB
+
+[input] tree.integer t
+
+A binary tree of integers.
+
+Guaranteed constraints:
+0 ≤ tree size < 5 · 104,
+-1000 ≤ node value ≤ 1000.
+
+[output] boolean
+
+Return true if t is symmetric and false otherwise.
+
+
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+def solution(t):
+
+    def is_mirror(left_subtree, right_subtree):
+        if not left_subtree and not right_subtree:
+            return True
+        if not left_subtree or not right_subtree:
+            return False
+        
+        return (left_subtree.value == right_subtree.value) and \
+               is_mirror(left_subtree.left, right_subtree.right) and \
+               is_mirror(left_subtree.right, right_subtree.left)
+    
+    if not t:
+        return True
+    
+    return is_mirror(t.left, t.right)
+        
+    
+
