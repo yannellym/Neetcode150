3ef4ce888cbb775e339c6544e4fa757af9c19787
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-09-12 20:04:19 -0400

    Create differenceBetweenOnesAndZerosInRowAndColumn.py

---
+++ differenceBetweenOnesAndZerosInRowAndColumn.py
@@ -0,0 +1,92 @@
+2482. Difference Between Ones and Zeros in Row and Column
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given a 0-indexed m x n binary matrix grid.
+
+A 0-indexed m x n difference matrix diff is created with the following procedure:
+
+Let the number of ones in the ith row be onesRowi.
+Let the number of ones in the jth column be onesColj.
+Let the number of zeros in the ith row be zerosRowi.
+Let the number of zeros in the jth column be zerosColj.
+diff[i][j] = onesRowi + onesColj - zerosRowi - zerosColj
+Return the difference matrix diff.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,1,1],[1,0,1],[0,0,1]]
+Output: [[0,0,4],[0,0,4],[-2,-2,2]]
+Explanation:
+- diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0 = 2 + 1 - 1 - 2 = 0 
+- diff[0][1] = onesRow0 + onesCol1 - zerosRow0 - zerosCol1 = 2 + 1 - 1 - 2 = 0 
+- diff[0][2] = onesRow0 + onesCol2 - zerosRow0 - zerosCol2 = 2 + 3 - 1 - 0 = 4 
+- diff[1][0] = onesRow1 + onesCol0 - zerosRow1 - zerosCol0 = 2 + 1 - 1 - 2 = 0 
+- diff[1][1] = onesRow1 + onesCol1 - zerosRow1 - zerosCol1 = 2 + 1 - 1 - 2 = 0 
+- diff[1][2] = onesRow1 + onesCol2 - zerosRow1 - zerosCol2 = 2 + 3 - 1 - 0 = 4 
+- diff[2][0] = onesRow2 + onesCol0 - zerosRow2 - zerosCol0 = 1 + 1 - 2 - 2 = -2
+- diff[2][1] = onesRow2 + onesCol1 - zerosRow2 - zerosCol1 = 1 + 1 - 2 - 2 = -2
+- diff[2][2] = onesRow2 + onesCol2 - zerosRow2 - zerosCol2 = 1 + 3 - 2 - 0 = 2
+Example 2:
+
+
+Input: grid = [[1,1,1],[1,1,1]]
+Output: [[5,5,5],[5,5,5]]
+Explanation:
+- diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0 = 3 + 2 - 0 - 0 = 5
+- diff[0][1] = onesRow0 + onesCol1 - zerosRow0 - zerosCol1 = 3 + 2 - 0 - 0 = 5
+- diff[0][2] = onesRow0 + onesCol2 - zerosRow0 - zerosCol2 = 3 + 2 - 0 - 0 = 5
+- diff[1][0] = onesRow1 + onesCol0 - zerosRow1 - zerosCol0 = 3 + 2 - 0 - 0 = 5
+- diff[1][1] = onesRow1 + onesCol1 - zerosRow1 - zerosCol1 = 3 + 2 - 0 - 0 = 5
+- diff[1][2] = onesRow1 + onesCol2 - zerosRow1 - zerosCol2 = 3 + 2 - 0 - 0 = 5
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 105
+1 <= m * n <= 105
+grid[i][j] is either 0 or 1.
+Accepted
+23.6K
+Submissions
+29.5K
+Acceptance Rate
+80.1%
+
+
+  class Solution(object):
+    def onesMinusZeros(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        rows = len(grid)  # Number of rows
+        cols = len(grid[0])  # Number of columns
+
+        onesRow = [0] * rows
+        onesCol = [0] * cols
+        zeroRow = [0] * rows
+        zeroCol = [0] * cols
+
+        for r in range(rows):
+            for c in range(cols):
+                onesRow[r] += grid[r][c]
+                onesCol[c] += grid[r][c]
+                zeroRow[r] += 1 - grid[r][c]
+                zeroCol[c] += 1- grid[r][c]
+
+        fgrid = [ [0] * cols for i in range(rows)]
+
+
+        for r in range(rows):
+            for c in range(cols):
+                fgrid[r][c] = onesRow[r] + onesCol[c] - zeroRow[r] - zeroCol[c]
+        return fgrid
+     
