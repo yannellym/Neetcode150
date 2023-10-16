880765a1b6c140d94e7d14a681644a337da25e61
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-10-16 17:21:00 -0400

    Update insertInterval.py

--- insertInterval.py
+++ insertInterval.py
@@ -38,6 +38,34 @@
 # 38.8%
 
 
+# clever solution
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        merged = []
+        i, n = 0, len(intervals)
+        # Add all intervals ending before newInterval starts
+        while i < n and intervals[i][1] < newInterval[0]:
+            merged.append(intervals[i])
+            i += 1
+        # Merge all intervals that overlap with newInterval
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+            i += 1
+        merged.append(newInterval)
+        # Add all intervals starting after newInterval ends
+        while i < n:
+            merged.append(intervals[i])
+            i += 1
+        return merged
+
+
+
 # new documentation
 
 class Solution(object):
