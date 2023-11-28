10768d1b6e73d1523755c07b7d6e74e674066407
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-11-27 20:08:24 -0500

    Update longestPalindromicSubstring.py

--- longestPalindromicSubstring.py
+++ longestPalindromicSubstring.py
@@ -61,3 +61,21 @@ class Solution(object):
         #   baba  -> 0 - 4
         #   abad -> 1 - 4
         #   bab -> 0 - 3
+
+
+# more efficient 
+
+  size = len(s)
+        # while size > 0 
+        while(size>0):
+            # loop length of s - (size + 1) times
+            for i in range(len(s) - size + 1):
+                # x will equal the substring of s from i to i plus size
+                x = s[i:i+size]
+                # if x equals the reverse of x return x
+                # this returns the first palindromic substring it sees.
+                # since're we starting with the entire array, it will automatically return the longest one
+                if x == x[::-1]:
+                    return x
+            # decrement size
+            size -= 1
