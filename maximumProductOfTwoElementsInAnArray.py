fa961df37fb0830b97ee8c68bd33085b7a7c292a
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2023-12-11 19:51:00 -0500

    Create maximumProductOfTwoElementsInAnArray.py

---
+++ maximumProductOfTwoElementsInAnArray.py
@@ -0,0 +1,58 @@
+1464. Maximum Product of Two Elements in an Array
+Solved
+Easy
+Topics
+Companies
+Hint
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,2]
+Output: 12 
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+Example 2:
+
+Input: nums = [1,5,4,5]
+Output: 16
+Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+Example 3:
+
+Input: nums = [3,7]
+Output: 12
+ 
+
+Constraints:
+
+2 <= nums.length <= 500
+1 <= nums[i] <= 10^3
+Seen this question in a real interview before?
+1/4
+
+
+import heapq
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        a = max(nums)
+        nums.remove(a)
+        b = max(nums)
+        return (a-1)*(b-1)
+
+# or
+
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return (nums[-2]-1)*(nums[-1]-1)
