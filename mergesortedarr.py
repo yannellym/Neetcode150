c36ba03c8811aaf6137fad8f35be9d940c175471
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2024-02-25 20:17:39 -0500

    Update mergesortedarr.py

--- mergesortedarr.py
+++ mergesortedarr.py
@@ -39,3 +39,27 @@ class Solution(object):
             else:
                 nums1[d] = nums2[p2]
                 p2 -= 1
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        l = m - 1
+        r = n - 1 
+        i = (m+n) - 1
+        while l >= 0 and r >= 0:
+            if nums1[l] > nums2[r]:
+                nums1[i] = nums1[l]
+                l -=1
+            else:
+                nums1[i] = nums2[r]
+                r -= 1
+            i -=1
+
+        nums1[:r+1] = nums2[:r+1] 
