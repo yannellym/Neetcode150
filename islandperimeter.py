ff7e362b8b4357297d0e2ea35b8a6ebd7a8dbc9b
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2024-04-18 19:40:06 -0400

    Update islandperimeter.py

--- islandperimeter.py
+++ islandperimeter.py
@@ -83,3 +83,36 @@ class Solution(object):
                         perimeter -= 1
         # return the final count of the perimeter
         return perimeter
+
+
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+       
+
+        """
+
+        rows = len(grid)
+        cols = len(grid[0])
+        peri = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    # check top
+                    if r == 0 or grid[r-1][c] == 0:
+                        peri += 1 
+                    # check left
+                    if c == 0 or grid[r][c-1] == 0:
+                        peri += 1
+                    # check right
+                    if c == cols - 1 or grid[r][c+1] == 0:
+                        peri += 1
+                    # check bottom
+                    if r == rows - 1 or grid[r+1][c] == 0:
+                        peri += 1    
+
+        # Perimeter value for all cells
+        return peri
