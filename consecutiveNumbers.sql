2ff59e50924d779e3d60f6635b2a2b16f0066165
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2024-08-22 15:40:02 -0400

    Create consecutiveNumbers.sql

---
+++ consecutiveNumbers.sql
@@ -0,0 +1,64 @@
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+
+# Write your MySQL query statement below
+
+SELECT DISTINCT
+    l1.Num AS ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+;
+
+
+# Write your MySQL query statement below
+SELECT DISTINCT num AS ConsecutiveNums
+FROM logs
+WHERE (id + 1, num) IN (SELECT * FROM logs)
+    AND (id + 2, num) IN (SELECT * FROM logs);
