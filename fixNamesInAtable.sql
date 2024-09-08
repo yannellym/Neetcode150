a6ff7b0c5452df4f2e95011b73d1def85616e42c
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2024-09-07 20:22:25 -0400

    Create fixNamesInAtable.sql

---
+++ fixNamesInAtable.sql
@@ -0,0 +1,60 @@
+Fix Names in a Table
+Solved
+Easy
+Topics
+Companies
+SQL Schema
+Pandas Schema
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+
+
+
+# Write your MySQL query statement below
+SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(RIGHT(name, LENGTH(name)-1))) AS name 
+FROM Users 
+ORDER BY user_id; 
+
+--or
+
+
+w
+SELECT user_id, 
+       CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) AS name
+FROM Users
+ORDER BY user_id;
