3fa83099cd0f0448f9f4ed7549d5ab12d9727171
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2024-09-10 19:59:59 -0400

    Create SecondHighestSalary.sql

---
+++ SecondHighestSalary.sql
@@ -0,0 +1,68 @@
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+
+
+# Write your MySQL query statement below
+
+-- SECOND HIGHEST DISTINCT SALARY
+-- IF NO SECOND HIGHEST, RETURN NULL
+
+
+-- if null, returns null
+SELECT IFNULL(
+    (
+    -- selects seocnd highest salary
+    SELECT DISTINCT(Salary) AS SecondHighestSalary
+    FROM Employee
+    ORDER BY Salary DESC
+    LIMIT 1,1
+    )
+, NULL) AS SecondHighestSalary
