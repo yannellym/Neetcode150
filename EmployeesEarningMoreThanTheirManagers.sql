91d92a2673a15049e45791909e29d8d163783083
Yannelly Mercado <91508647+yannellym@users.noreply.github.com>
2024-09-12 16:22:51 -0400

    Create EmployeesEarningMoreThanTheirManagers.sql

---
+++ EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,60 @@
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+Write a solution to find the employees who earn more than their managers.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+-----------+
+| id | name  | salary | managerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | Null      |
+| 4  | Max   | 90000  | Null      |
++----+-------+--------+-----------+
+Output: 
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+Explanation: Joe is the only employee who earns more than his manager.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+690.2K
+Submissions
+983.5K
+Acceptance Rate
+70.2%
+
+
+# Write your MySQL query statement below
+
+SELECT e1.name as Employee
+FROM Employee as e1
+JOIN Employee as e2
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary
